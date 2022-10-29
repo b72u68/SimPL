22c50ad4748864978330ca8e033774ed8978aea7
b72u68 <mdinh@hawk.iit.edu>
2022-10-29 16:30:12 -0500

    Only opened parse module

--- bin/simpl.ml
+++ bin/simpl.ml
@@ -1,9 +1,9 @@
-open SimPL
+open SimPL.Parse
 
 let filename = Sys.argv.(1);;
 
 let _ =
     let filetype = Filename.extension filename in
     if filetype <> ".spl" then failwith (Printf.sprintf "Unsupported file type '%s'. Expected '.spl'." filetype)
-    else Parse.parse filename
+    else parse filename ~source: FILE
 ;;
