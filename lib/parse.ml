05c714a1dbe094ab374c16463f65c5199ddfd272
b72u68 <mdinh@hawk.iit.edu>
2022-11-08 16:57:36 -0600

    Closed in channel after reading a file

--- lib/parse.ml
+++ lib/parse.ml
@@ -1,11 +1,18 @@
 type parsesrc = FILE | STRING
 
 let parse ?(source=FILE) (input: string) =
-    let lexbuf =
-        match source with
-        | FILE -> open_in input |> Lexing.from_channel
-        | STRING -> Lexing.from_string input
-    in
-    let ast = Parser.prog Lexer.token lexbuf in
-    ast
+    match source with
+    | FILE ->
+            let chan = open_in input in
+            (try
+                let lexbuf = Lexing.from_channel chan in
+                let ast = Parser.prog Lexer.token lexbuf in
+                close_in chan;
+                ast
+            with e ->
+                close_in_noerr chan;
+                raise e)
+    | STRING ->
+            let lexbuf = Lexing.from_string input in
+            Parser.prog Lexer.token lexbuf
 ;;
